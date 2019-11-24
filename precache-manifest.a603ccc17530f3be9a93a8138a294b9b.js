f7948eab81a6219520a51551ca8128869454c06e
gottedev <gottedev@gmail.com>
2019-11-24 00:05:13 +0000

    Updates

---
+++ precache-manifest.a603ccc17530f3be9a93a8138a294b9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f12f8150963e5a6e0a099688e095002",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "53dba2defc617d8d3796",
+    "url": "/carousel/static/css/4.dd17b5ef.chunk.css"
+  },
+  {
+    "revision": "67b8ad936c7dc46a8cd5",
+    "url": "/carousel/static/css/main.ea5cf213.chunk.css"
+  },
+  {
+    "revision": "599f863d84cc4243f98a",
+    "url": "/carousel/static/js/2.37cb1efa.chunk.js"
+  },
+  {
+    "revision": "5dc77b4dec0294b013fb",
+    "url": "/carousel/static/js/3.42344d17.chunk.js"
+  },
+  {
+    "revision": "53dba2defc617d8d3796",
+    "url": "/carousel/static/js/4.5fbd96b6.chunk.js"
+  },
+  {
+    "revision": "67b8ad936c7dc46a8cd5",
+    "url": "/carousel/static/js/main.0ef75901.chunk.js"
+  },
+  {
+    "revision": "1d6e633e37a08485adc6",
+    "url": "/carousel/static/js/runtime-main.006555aa.js"
+  }
+]);
